2dd29d0ce34942c00eb5a0bea0162cf7bbcf0263
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2013-07-31 07:43:18 -0400

    Updated the config_local system to please git's ignoring capabilities

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
+# Don't edit this file. Create a config_local.py
+# to the project root and it will override any values
+# here.
+
 server_port = 8080
+server_host = "127.0.0.1"
 
 coordinate_projection = "epsg:3067"
 db_connection_uri = "postgres://transit:transit@localhost/transit"
 
-import config_local
-for k in dir(config_local):
-	if k.startswith('_'): continue
-	locals()[k] = getattr(config_local, k)
-
+try:
+	import config_local
+	for k in dir(config_local):
+		if k.startswith('_'): continue
+		locals()[k] = getattr(config_local, k)
+except ImportError:
+	pass
