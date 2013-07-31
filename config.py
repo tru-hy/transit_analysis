622fcd49754a8fc4ed5cd0d2c3c74cf0b5f9eac9
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2013-07-31 07:29:59 -0400

    Added local configuration system

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+server_port = 8080
+
 coordinate_projection = "epsg:3067"
-db_connection_uri = "postgres://transit:transit@/transit"
+db_connection_uri = "postgres://transit:transit@localhost/transit"
+
+import config_local
+for k in dir(config_local):
+	if k.startswith('_'): continue
+	locals()[k] = getattr(config_local, k)
+
